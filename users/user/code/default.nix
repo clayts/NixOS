78aced6ab2575b8decf49ebf4b6e0ce22f614c0a
clayts <clayton.spicer@gmail.com>
2024-06-09 09:00:18 +0100

    ultra mega meta hack for Code settings

--- users/user/code/default.nix
+++ users/user/code/default.nix
@@ -16,9 +16,12 @@
       rust-lang.rust-analyzer
     ];
   };
+  # MEGAHACK if this flake is installed to /etc/nixos, link to ./settings.json directly, allowing settings to be changed in the app and recorded in version control. Otherwise link the file from the store immutably as normal.
+  # To disable this, comment out the home.activation = {...}; section, and uncomment the following line:
+  # home.file.".config/Code/User/settings.json".source = ./settings.json;
   home.activation = {
     link-code-settings = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      run mkdir -p $HOME/.config/Code/User && ln -s /etc/nixos/users/user/code/settings.json $HOME/.config/Code/User/settings.json
+      run mkdir -p $HOME/.config/Code/User && if [ ! -f /etc/nixos/users/user/code/settings.json ]; then ln -s /etc/nixos/users/user/code/settings.json $HOME/.config/Code/User/settings.json; else ln -s ${./settings.json} $HOME/.config/Code/User/settings.json;fi
     '';
   };
 }
