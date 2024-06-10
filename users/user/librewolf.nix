4593b2019b7081b7fd3f2642b210b85b3d804282
clayts <clayton.spicer@gmail.com>
2024-06-10 09:35:07 +0100

    autofill credit cards and addresses

--- users/user/librewolf.nix
+++ users/user/librewolf.nix
@@ -4,5 +4,7 @@
     "identity.fxaccounts.enabled" = true;
     "privacy.clearOnShutdown.history" = false;
     "privacy.clearOnShutdown.downloads" = false;
+    "extensions.formautofill.addresses.enabled" = true;
+    "extensions.formautofill.creditCards.enabled" = true;
   };
 }
