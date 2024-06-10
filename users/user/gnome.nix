e0cc0d4ed741703d63730acc449ac185694a7429
clayts <clayton.spicer@gmail.com>
2024-06-10 10:03:44 +0100

    stopped the daily beep!!

--- users/user/gnome.nix
+++ users/user/gnome.nix
@@ -4,5 +4,8 @@
     "org/gnome/shell" = {
       favorite-apps = ["org.gnome.Nautilus.desktop" "org.gnome.Console.desktop"];
     };
+    "org/gnome/evolution-data-server.calendar" = {
+      notify-enable-audio = false;
+    };
   };
 }
