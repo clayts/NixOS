8766126caf3315eec9598a374aa31fa55800e4e6
clayts <clayton.spicer@gmail.com>
2024-06-11 04:25:01 +0100

    only make system steam-ready if a user wants to install steam

--- os/steam-ready.nix
+++ os/steam-ready.nix
@@ -1,8 +1,21 @@
-{pkgs, ...}: {
-  programs.steam = {
-    enable = true;
-    package = pkgs.steamPackages.steam-fhsenv-without-steam; # Just install everything except for steam, let steam itself be installed by each user individually
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  };
+{
+  pkgs,
+  config,
+  ...
+}: {
+  programs.steam =
+    if
+      (
+        builtins.any (user: (builtins.any (package: package == pkgs.steam) user.packages)) (builtins.attrValues config.users.users)
+      )
+      || (
+        builtins.any (user: (builtins.any (package: package == pkgs.steam) user.home.packages)) (builtins.attrValues config.home-manager.users)
+      )
+    then {
+      enable = true;
+      package = pkgs.steamPackages.steam-fhsenv-without-steam; # Just install everything except for steam, let steam itself be installed by each user individually
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    }
+    else {};
 }
