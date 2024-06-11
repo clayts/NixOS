62e3dc7f856da6c4df00a2af8dc689790439e87d
clayts <clayton.spicer@gmail.com>
2024-06-11 03:21:14 +0100

    moved gnome-wallpaper-fetch out of common

--- users/common/gnome/default.nix
+++ users/common/gnome/default.nix
@@ -4,31 +4,25 @@
   ...
 }: {
   home.packages = let
-    wallpaper-fetch = pkgs.writeShellScriptBin "gnome-wallpaper-fetch" (builtins.readFile ./gnome-wallpaper-fetch.sh);
     rounded-window-corners-reborn = pkgs.callPackage ./rounded-window-corners-reborn.nix {};
   in
     with pkgs; [
       bibata-cursors
       papirus-icon-theme
-      wallpaper-fetch
       rounded-window-corners-reborn
       gnomeExtensions.grand-theft-focus
       gnomeExtensions.appindicator
 
       # Apps
       gnome-console
-      gnome.baobab
       gnome.cheese
       loupe
       gnome.file-roller
       gnome.gnome-calculator
-      gnome.gnome-logs
       gnome.gnome-system-monitor
       gnome.gnome-characters
       gnome.gnome-screenshot
-      gnome.gnome-disk-utility
       gnome.nautilus
-      gnome.gnome-calendar
       celluloid
       gnome-text-editor
     ];
@@ -36,14 +30,7 @@
     name = "";
     noDisplay = true;
   };
-  home.file.".config/autostart/gnome-wallpaper-fetch.desktop".text = ''
-    [Desktop Entry]
-    Exec=sh -c "while true; do sleep 12h; gnome-wallpaper-fetch; done"
-    Name=Wallpaper Fetcher
-    Terminal=false
-    Type=Application
-    Version=1.4
-  '';
+
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       font-name = "${(builtins.elemAt config.fonts.fontconfig.defaultFonts.sansSerif 0) + " " + "11"}";
@@ -65,8 +52,6 @@
     "org/gnome/shell" = {
       favorite-apps = [
         "librewolf.desktop"
-        "org.gnome.Nautilus.desktop"
-        "org.gnome.Console.desktop"
       ];
       enabled-extensions = [
         "grand-theft-focus@zalckos.github.com"
---
+++ users/user/gnome/default.nix
@@ -0,0 +1,28 @@
+{pkgs, ...}: {
+  home.packages = let
+    gnome-wallpaper-fetch = pkgs.writeShellScriptBin "gnome-wallpaper-fetch" (builtins.readFile ./gnome-wallpaper-fetch.sh);
+  in
+    with pkgs; [
+      gnome-wallpaper-fetch
+      gnome.baobab
+      gnome.gnome-logs
+      gnome.gnome-disk-utility
+      gnome.gnome-calendar
+    ];
+  home.file.".config/autostart/gnome-wallpaper-fetch.desktop".text = ''
+    [Desktop Entry]
+    Exec=sh -c "while true; do sleep 12h; gnome-wallpaper-fetch; done"
+    Name=Wallpaper Fetcher
+    Terminal=false
+    Type=Application
+    Version=1.4
+  '';
+  dconf.settings = {
+    "org/gnome/shell" = {
+      favorite-apps = ["org.gnome.Nautilus.desktop" "org.gnome.Console.desktop"];
+    };
+    "org/gnome/evolution-data-server.calendar" = {
+      notify-enable-audio = false; # Silences annoying daily beeps
+    };
+  };
+}
