2b49e2222b25180a7d4a8190222e04ccbc93e3eb
clayts <clayton.spicer@gmail.com>
2024-06-14 10:11:53 +0100

    cleanup

--- users/common/nix.nix
+++ users/common/nix.nix
@@ -1,5 +1,9 @@
 {
-  nix.gc.automatic = true;
-  nix.gc.frequency = "weekly";
-  nix.gc.options = "--delete-older-than 7d";
+  nix = {
+    gc = {
+      automatic = true;
+      frequency = "weekly";
+      options = "--delete-older-than 7d";
+    };
+  };
 }
