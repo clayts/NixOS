d7f4b2b6e3955223c0cccfd1a91737e6ff313088
clayts <clayton.spicer@gmail.com>
2024-06-14 05:14:02 +0100

    CRT bugfixes

--- users/user/crt/default.nix
+++ users/user/crt/default.nix
@@ -1,8 +1,6 @@
 {...}: {
   xdg.desktopEntries.crt = {
     name = "CRT";
-    exec = ''
-      TRUE_XDG_CONFIG_HOME=$XDG_CONFIG_HOME TRUE_XDG_DATA_HOME=$XDG_DATA_HOME nix shell nixpkgs#cool-retro-term -c sh -c "XDG_CONFIG_HOME=${./settings} XDG_DATA_HOME=${./settings} cool-retro-term --fullscreen -e sh -c \"SHLVL=0 XDG_CONFIG_HOME=$TRUE_XDG_CONFIG_HOME XDG_DATA_HOME=$TRUE_XDG_DATA_HOME; cd; exec zsh\""
-    '';
+    exec = ''sh -c "${./crt.sh} ${./settings}"'';
   };
 }
