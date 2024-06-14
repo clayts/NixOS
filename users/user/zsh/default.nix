30dcf3e56d23a0bc3ff333c53b0ceab4a2e5361b
clayts <clayton.spicer@gmail.com>
2024-06-14 10:12:02 +0100

    pkg-tree

--- users/user/zsh/default.nix
+++ users/user/zsh/default.nix
@@ -18,6 +18,7 @@ in {
     la = "eza -al --icons=always --time-style=relative --color-scale-mode=gradient --color-scale --group-directories-first";
     cd = "z";
     ls-new = "find . -type f -printf '%BF %BT %p\n' | sort";
+    pkg-tree = "function _pkg-tree() { lt $(nix build $* --print-out-paths --no-link); }; _pkg-tree";
   };
   home.sessionVariables = {
     EDITOR = "micro";
