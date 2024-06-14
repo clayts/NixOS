2c4e8b5d12bfcddb2d4d216253c3f2395320075d
clayts <clayton.spicer@gmail.com>
2024-06-14 17:40:38 +0100

    tweak crt settings

--- users/user/crt/crt.sh
+++ users/user/crt/crt.sh
@@ -1,7 +1,6 @@
 TMP=$(mktemp -d)
-echo "temp:" $TMP
 echo "copying $1 to $TMP"
-cp -R "$1" "$TMP"/crt
-echo nix shell nixpkgs#cool-retro-term -c sh -c "XDG_CONFIG_HOME=$TMP/crt XDG_DATA_HOME=$TMP/crt cool-retro-term --fullscreen -e sh -c \"SHLVL=0; unset XDG_CONFIG_HOME; unset XDG_DATA_HOME; cd; exec zsh\""
-nix shell nixpkgs#cool-retro-term -c sh -c "XDG_CONFIG_HOME=$TMP/crt XDG_DATA_HOME=$TMP/crt cool-retro-term --fullscreen -e sh -c \"SHLVL=0; unset XDG_CONFIG_HOME; unset XDG_DATA_HOME; cd; exec zsh\""
+cp -R "$1"/* "$TMP"
+# TMP=./settings
+nix shell nixpkgs#cool-retro-term -c sh -c "XDG_CONFIG_HOME=$TMP XDG_DATA_HOME=$TMP cool-retro-term --fullscreen -e sh -c \"SHLVL=0; unset XDG_CONFIG_HOME; unset XDG_DATA_HOME; cd; exec zsh\""
 rm -Rf $TMP
